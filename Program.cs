6ace338b220f5a45ef4916cf4fdebbe92eace440
SofiBerumen <sberumen@redlab.com.mx>
2018-10-05 16:48:01 -0500

    Primero

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace suma_de_multiplos_de_3_y_5_SofiBerumen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dame numero");
+            String tam=Console.ReadLine();
+            int n;
+            if(Int32.TryParse (tam, out n)){
+                Console.WriteLine("El numero es entero");
+                int sum=0;
+
+                for(int x=1; x<=n; x++){
+                    if(x%3==0 || x%5==0){
+                        sum= sum+x;
+                        Console.Write("MULTIPLOS: "+x+"\n");
+                    }
+                    
+                }
+                Console.WriteLine("** SUMA: ** " + sum);
+            
+            } else {
+                Console.WriteLine("El numero es entero");
+            }
+
+
+
+
+        }
+    }
+}
